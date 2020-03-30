34c6b243bcf99142e8e00a5ecdc33963343f88b8
Roberto Robson <betorobson@gmail.com>
2020-03-30 09:32:26 -0300

    network bridge to hide whoami service

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.get('/app1', (req, res) => res.json({
 	message: 'app1'
 }));
+
+app.get(
+  '/app1-whoami',
+  (req, res) => {
+
+    axios.request({
+      url : 'http://whoami/whoami',
+      method: 'GET'
+    }).then(response =>
+
+      res.json({
+        message: 'app1-whoami',
+        data: response.data.split('\r\n')
+      })
+
+    )
+    .catch(error => {
+
+      res.json({
+        message: 'app1-whoami',
+        error: error
+      });
+
+    });
+
+  }
+);
+
+// whoami
 
 app.listen(3000);
