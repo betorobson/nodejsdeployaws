7855ea8e8a373e1500211555feabc43116a63b20
Roberto Robson <betorobson@gmail.com>
2020-04-03 15:52:12 -0300

    wrapper middewares

--- middlewares/wrapper-async-middeware-exceptions.js
+++ middlewares/wrapper-async-middeware-exceptions.js
@@ -3,15 +3,24 @@ const { wrapperMiddlewareExceptions } = require('./wrapper-middleware-exceptions
 // if it fill res.jsonBody with result of any DataBase Query
 // and call next()
 
-exports.wrapperDBQueryControllerAsMiddleware = controller =>
+exports.wrapperAsyncMiddlewareExceptions = promise =>
 	wrapperMiddlewareExceptions(
 		(req, res, next) => {
-			controller(req, res, next)
+      promise()
 				.then(data => {
 					res.jsonBody = data,
 					next();
 				})
 				.catch(err => {
-					next(err);
+
+          var errorObj = {
+            message: err.message
+          };
+
+          if(req.query['_debug_'] === 'true'){
+            errorObj.stack = (err.stack || '').split('\n');
+          }
+
+          next({message: errorObj});
 				});
 		});
